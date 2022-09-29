f52eb8811b62486d5eae688d566265d9448e55b7
amit4106 <amitkumarbharti4106@gmail.com>
2022-09-29 11:03:05 +0530

    create get api

--- mywebapp/src/main/java/com/demo/webapp/controller/EmployeeController.java
+++ mywebapp/src/main/java/com/demo/webapp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.demo.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,7 @@ public class EmployeeController {
 	}
 	
 	
+	@GetMapping(value = "/findById")
 	public ResponseEntity<?> findById(@PathVariable Integer id){
 		return null;
 	}
